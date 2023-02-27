aa1f0f7da0835dfac9027af89d8b20a5b28b6cdb
visal-99 <sal061710313@gmail.com>
2023-02-27 11:10:13 +0700

    0

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
-const { Telegraf } = require('telegraf');
-const TOKEN="5803362552:AAEqtKjbGNfIailrUXtxrHZXEmj6Yv9mv9I";
-const bot = new Telegraf(TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5803362552:AAEqtKjbGNfIailrUXtxrHZXEmj6Yv9mv9I';
+const bot = new TelegramBot(token, { polling: true });
 
+// Create payment button
+bot.on('message', (msg) => {
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Pay',
+            pay: true,
+          },
+        ],
+      ],
+    },
+  };
+  bot.sendMessage(msg.chat.id, 'Would you like to pay?', opts);
+});
 
-bot.launch();
+// Handle payment confirmation
+bot.on('pre_checkout_query', (query) => {
+  bot.answerPreCheckoutQuery(query.id, true);
+});
