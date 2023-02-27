19b3d30ff9c49e8ed6a1e46f7d2a6cec5e4ec101
visal-99 <sal061710313@gmail.com>
2023-02-27 09:29:23 +0700

    -

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const { Telegraf } = require('telegraf');
 const TOKEN="5803362552:AAEqtKjbGNfIailrUXtxrHZXEmj6Yv9mv9I";
 const bot = new Telegraf(TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 
 bot.launch();
