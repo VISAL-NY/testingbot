ac10c4966960730fda6b85e264ffbd8bd0e4aa4f
visal-99 <sal061710313@gmail.com>
2023-03-02 11:36:11 +0700

    0

--- emvtest.js
+++ emvtest.js
@@ -2,7 +2,8 @@ const emvqr=require('emvqr')
 
 function onOpenScan(){
     tele.showScanQrPopup("text",function(qr){
-    const result=emvqr.decode(qr);
-    alert(result);
+        alert(qr);
+        const result=emvqr.decode(qr);
+        alert(result);
 });
 }
