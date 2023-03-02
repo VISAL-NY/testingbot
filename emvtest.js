113ce7916ce1a3b91d87a17c72ec4a3b8825b3b6
visal-99 <sal061710313@gmail.com>
2023-03-02 16:25:58 +0700

    0

--- emvtest.js
+++ emvtest.js
@@ -1,9 +1,17 @@
-const {emvqrcode}=require('emvqr/emvqr')
+const emvqrcode =require('emvqr');
+
+//const example="00020101021229300012D156000000000510A93FO3230Q31280012D15600000001030812345678520441115802CN5914BEST TRANSPORT6007BEIJING64200002ZH0104最佳运输0202北京540523.7253031565502016233030412340603***0708A60086670902ME91320016A0112233449988770708123456786304A13A";
+
+//const result=emvqrcode.decode(example);
+
+//console.log(result);
+
+
+
 
 function onOpenScan(){
-    tele.showScanQrPopup("text",function(qr){
-        alert(qr);
-        const result=emvqrcode.decode(qr);
+    tele.showScanQrPopup("text",function(qrcode){
+        const result=emvqrcode.decode(qrcode);
         console.log(result);
 });
 }
